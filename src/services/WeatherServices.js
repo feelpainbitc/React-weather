442a5885d3efb7ebdeecb060dcea5480a20b344c
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-04-08 20:55:27 +0300

    adding APIS(may be fault)

--- src/services/WeatherServices.js
+++ src/services/WeatherServices.js
@@ -1,5 +1,8 @@
+import axios from "axios";
+
+
 export class WeatherService{
     static getCurrentWeather(city){
-        return axios.get('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={32be222386cd98c762b5ab0c911ea4cc}')
+        return axios.get(`https://api.openweathermap.org/data/3.0/onecall?${city}&lat={53.669353}&lon={23.813131}&appid={32be222386cd98c762b5ab0c911ea4cc}`)
     }
 }
---
+++ src/store/thunks/fetchCurrentWeather.js
@@ -0,0 +1,4 @@
+export const fetchCurrentWeather=(payload)=>(dispatch:AppDispatch)=>{
+
+}
+    
